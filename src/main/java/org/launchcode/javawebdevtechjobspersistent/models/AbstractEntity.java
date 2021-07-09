85e3b683e77d999f9717f4127426a30df3826e4a
Joe Knapp <jtkgy5@gmail.com>
2021-07-09 07:21:07 -0500

    First

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/AbstractEntity.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/AbstractEntity.java
@@ -1,12 +1,22 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
 
+@MappedSuperclass
 public abstract class AbstractEntity {
 
+    @Id
+    @GeneratedValue
     private int id;
 
+    @NotBlank (message = "Entry is required")
+    @Size (min = 1, max = 100, message = "Entry must be between 1 and 100 characters")
     private String name;
 
     public int getId() {
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,16 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
+
+    @NotBlank (message = "Entry must be between 5 and 500 characters")
+    @Size(min = 5, max = 500)
+    private String description;
+
+    public Skill(){}
 
 }

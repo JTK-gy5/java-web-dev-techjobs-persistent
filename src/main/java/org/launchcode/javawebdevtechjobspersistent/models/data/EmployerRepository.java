1dc4d7bcf6e54bd17c0bfbcef3f9c774c442b804
Joe Knapp <jtkgy5@gmail.com>
2021-07-14 18:55:39 -0500

    Repository interfaces created

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/data/EmployerRepository.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/data/EmployerRepository.java
@@ -1,4 +1,10 @@
 package org.launchcode.javawebdevtechjobspersistent.models.data;
 
-public interface EmployerRepository {
+import org.launchcode.javawebdevtechjobspersistent.models.Employer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployerRepository extends CrudRepository<Employer, Integer> {
 }
